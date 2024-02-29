44867213d112d578d0c67e388f0b5ed0f0f4b571
Brendon Victor <devbrendonvs@gmail.com>
2024-02-29 14:57:46 -0300

    main.tsx configurado

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ContextProvider } from './Context/index.tsx'
+import { CssBaseline } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ContextProvider>
     <App />
+    <CssBaseline/>
+    </ContextProvider>
   </React.StrictMode>,
 )
