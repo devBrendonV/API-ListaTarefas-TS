bd1588a2fc00443b0fbc832f911137c5abbde868
Brendon Victor <devbrendonvs@gmail.com>
2024-03-03 12:01:24 -0300

    Components/DeletarLista configurado

--- src/Components/Lista/DeletarLista.tsx
+++ src/Components/Lista/DeletarLista.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { Context } from "../../Context";
+import { useContext, useState } from "react";
+import { IconButton } from "@mui/material";
 
-export const DeletarLista = () => {
+export default function DeletarLista() {
+  const { apagar, idAtual } = useContext(Context);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>DeletarLista</div>
-  )
+    <>
+      <IconButton
+        title="Deletar Lista"
+        sx={{
+          color: "#d80909",
+        }}
+        onClick={handleClickOpen}
+      >
+        <DeleteForeverIcon />
+      </IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Deseja realmente excluir essa lista?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Essa ação não pode ser revertida.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => apagar(idAtual)}>Sim</Button>
+          <Button onClick={handleClose}>Não</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
